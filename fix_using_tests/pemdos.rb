f9bf0f48f48a34b9afcbbebff1fd767aa436ca6f
Akiko Green <akikogreen08@gmail.com>
2020-04-24 01:18:35 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,10 @@
-# don't forget to add: require 'pry'
+ require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+    's' * 10 + string
   else
-    string
+   return string
   end
 end
+# binding.pry
